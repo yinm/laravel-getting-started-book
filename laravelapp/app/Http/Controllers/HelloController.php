a9c6b045d2b85a43dfd313e9cb703c4a452d2ed1
yinm <yinm.cminor9@gmail.com>
2017-09-16 17:15:15 +0900

    Use action method

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -7,36 +7,8 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index(Request $request, Response $response)
+    public function index()
     {
-        $html = <<<EOF
-<html>
-<head>
-  <title>Hello/Index</title>
-  <style>
-    body {
-      font-size: 16pt;
-      color: #999;
-    }
-    h1 {
-      font-size: 120pt;
-      text-align: right;
-      color: #fafafa;
-      margin: -50px 0 -120px 0;
-    }
-  </style>
-</head>
-<body>
-  <h1>Hello</h1>
-  <h3>Request</h3>
-  <pre>{$request}</pre>
-  <h3>Response</h3>
-  <pre>{$response}</pre>
-</body>
-</html>
-EOF;
-
-        $response->setContent($html);
-        return $response;
+        return view('hello.index');
     }
 }
--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -15,7 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', function() {
-    return view('hello.index');
-});
+Route::get('hello', 'HelloController@index');
 
