b08a3f724687568a9d7d762ba46c82a16a073d2b
yinm <yinm.cminor9@gmail.com>
2017-09-16 16:16:20 +0900

    Add route parameters

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    public function index()
+    public function index($id = 'noname', $pass = 'unknown')
     {
         return <<<EOF
 <html>
@@ -28,6 +28,10 @@ class HelloController extends Controller
 <body>
   <h1>Index</h1>
   <p>これは、Helloコントローラのindexアクションです。</p>
+  <ul>
+    <li>ID: {$id}</li>
+    <li>PASS: {$pass}</li>
+  </ul>
 </body>
 </html>
 EOF;
