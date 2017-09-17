b3e79b9df7fb70e98a377196e1002a8ba4a86d9f
yinm <yinm.cminor9@gmail.com>
2017-09-17 15:10:18 +0900

    Add post action

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -10,9 +10,18 @@ class HelloController extends Controller
     public function index()
     {
         $data = [
-            'msg' => 'これはBladeを利用したサンプルです。'
+            'msg' => 'お名前を入力してください。'
         ];
 
+        return view('hello.index', $data);
+    }
+
+    public function post(Request $request)
+    {
+        $msg = $request->msg;
+        $data = [
+            'msg' => 'こんにちは、' . $msg . 'さん！'
+        ];
         return view('hello.index', $data);
     }
 }
--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -16,3 +16,4 @@ Route::get('/', function () {
 });
 
 Route::get('hello', 'HelloController@index');
+Route::post('hello', 'HelloController@post');
