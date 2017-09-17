63e0f80284e00714acb12b06d102368fd6eca450
yinm <yinm.cminor9@gmail.com>
2017-09-17 16:49:46 +0900

    Add View::composer

--- laravelapp/app/Providers/HelloServiceProvider.php
+++ laravelapp/app/Providers/HelloServiceProvider.php
@@ -1,28 +1,17 @@
 <?php
-
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class HelloServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //
-    }
-
-    /**
-     * Register the application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
+        View::composer(
+            'hello.index', function($view) {
+                $view->with('view_message', 'composer message!');
+            }
+        );
     }
 }
