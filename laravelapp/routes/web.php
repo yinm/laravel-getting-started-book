697485b7329b1e1390b61d4c8064acbbd4630f89
yinm <yinm.cminor9@gmail.com>
2017-09-18 08:58:06 +0900

    Add global middleware

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -16,7 +16,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', 'HelloController@index')
-     ->middleware(HelloMiddleware::class);
+Route::get('hello', 'HelloController@index');
 
 Route::post('hello', 'HelloController@post');
