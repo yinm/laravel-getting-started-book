008b3604db6441e27416d66dd0a0f2942791c2c2
yinm <yinm.cminor9@gmail.com>
2017-09-18 16:06:14 +0900

    Add guarded and validatie

--- laravelapp/app/Person.php
+++ laravelapp/app/Person.php
@@ -7,6 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
+    protected $guarded = [
+        'id'
+    ];
+
+    public static $rules = [
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150'
+    ];
+
     public function getData()
     {
         return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
