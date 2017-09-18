9d6b95418c9e89bfee3974b58e55f3550e35868a
yinm <yinm.cminor9@gmail.com>
2017-09-18 11:47:36 +0900

    Add concat parameter

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -10,7 +10,15 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $items = DB::select('select * from people');
+        if (isset($request->id)) {
+            $params = [
+                'id' => $request->id
+            ];
+            $items = DB::select('select * from people where id = :id', $params);
+        } else {
+            $items = DB::select('select * from people');
+        }
+
         return view('hello.index', ['items' => $items]);
     }
 
