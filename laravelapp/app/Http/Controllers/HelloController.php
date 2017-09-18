f608168e5df995d5cd1165a65b7a44aeb0992177
yinm <yinm.cminor9@gmail.com>
2017-09-18 12:26:05 +0900

    Use DB::delete

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -86,4 +86,24 @@ class HelloController extends Controller
 
         return redirect('/hello');
     }
+
+    public function del(Request $request)
+    {
+        $params = [
+            'id' => $request->id
+        ];
+        $item = DB::select('select * from people where id = :id', $params);
+
+        return view('hello.del', ['form' => $item[0]]);
+    }
+
+    public function remove(Request $request)
+    {
+        $params = [
+            'id' => $request->id
+        ];
+        DB::delete('delete from people where id = :id', $params);
+
+        return redirect('/hello');
+    }
 }
--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -24,3 +24,6 @@ Route::post('hello/add', 'HelloController@create');
 
 Route::get('hello/edit', 'HelloController@edit');
 Route::post('hello/edit', 'HelloController@update');
+
+Route::get('hello/del', 'HelloController@del');
+Route::post('hello/del', 'HelloController@remove');
