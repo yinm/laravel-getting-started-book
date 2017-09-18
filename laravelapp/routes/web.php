48310f271f66ba8a101d79e3440212d1ff1926a5
yinm <yinm.cminor9@gmail.com>
2017-09-18 09:08:25 +0900

    Add group middleware

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -16,6 +16,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', 'HelloController@index');
+Route::get('hello', 'HelloController@index')
+     ->middleware('helo');
 
 Route::post('hello', 'HelloController@post');
