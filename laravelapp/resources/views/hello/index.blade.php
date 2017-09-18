0796e93c15b0147cd7619ee2cb21a90317b7495f
yinm <yinm.cminor9@gmail.com>
2017-09-18 09:27:29 +0900

    Add error messages and old value

--- laravelapp/resources/views/hello/index.blade.php
+++ laravelapp/resources/views/hello/index.blade.php
@@ -9,20 +9,30 @@
 
 @section('content')
   <p>{{ $msg }}</p>
+  @if (count($errors) > 0)
+    <div>
+      <ul>
+        @foreach ($errors->all() as $error)
+          <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+  @endif
+
   <table>
     <form action="/hello" method="post">
       {{ csrf_field() }}
       <tr>
         <th>name:</th>
-        <td><input type="text" name="name"></td>
+        <td><input type="text" name="name" value="{{old('name')}}"></td>
       </tr>
       <tr>
         <th>mail:</th>
-        <td><input type="text" name="mail"></td>
+        <td><input type="text" name="mail" value="{{old('mail')}}"></td>
       </tr>
       <tr>
         <th>age: </th>
-        <td><input type="text" name="age"></td>
+        <td><input type="text" name="age" value="{{old('age')}}"></td>
       </tr>
       <tr>
         <td><input type="submit" value="send"></td>
