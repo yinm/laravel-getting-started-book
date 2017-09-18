ce9e3e93d72ec5eb26cd1993159784f2cee4a42e
yinm <yinm.cminor9@gmail.com>
2017-09-18 13:47:24 +0900

    Update migration file

--- laravelapp/database/migrations/2017_09_18_044229_create_people_table.php
+++ laravelapp/database/migrations/2017_09_18_044229_create_people_table.php
@@ -15,6 +15,9 @@ class CreatePeopleTable extends Migration
     {
         Schema::create('people', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('mail');
+            $table->integer('age');
             $table->timestamps();
         });
     }
