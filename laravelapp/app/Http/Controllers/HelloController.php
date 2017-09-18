9ddb1a22b4838735cbde3e1d203b13c68190f325
yinm <yinm.cminor9@gmail.com>
2017-09-18 12:49:26 +0900

    Use relational operator in where method

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -102,8 +102,8 @@ class HelloController extends Controller
     public function show(Request $request)
     {
         $id = $request->id;
-        $item = DB::table('people')->where('id', $id)->first();
+        $items = DB::table('people')->where('id', '<=', $id)->get();
 
-        return view('hello.show', ['item' => $item]);
+        return view('hello.show', ['items' => $items]);
     }
 }
