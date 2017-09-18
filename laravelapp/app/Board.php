f8cd2c4f5f84ddada762bdd263f22624eaca1af9
yinm <yinm.cminor9@gmail.com>
2017-09-18 17:03:31 +0900

    Update model

--- laravelapp/app/Board.php
+++ laravelapp/app/Board.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Board extends Model
 {
-    //
+    protected $guarded = [
+        'id'
+    ];
+
+    public static $rules = [
+        'person_id' => 'required',
+        'title' => 'required',
+        'message' => 'required'
+    ];
+
+    public function getData()
+    {
+        return $this->id . ': ' . $this->title;
+    }
 }
