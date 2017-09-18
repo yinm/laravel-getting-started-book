8388503517dba9cd6d3f0a00583dcac3f17b68a4
yinm <yinm.cminor9@gmail.com>
2017-09-18 09:34:19 +0900

    Update rendering error messages

--- laravelapp/resources/views/hello/index.blade.php
+++ laravelapp/resources/views/hello/index.blade.php
@@ -10,30 +10,46 @@
 @section('content')
   <p>{{ $msg }}</p>
   @if (count($errors) > 0)
-    <div>
-      <ul>
-        @foreach ($errors->all() as $error)
-          <li>{{ $error }}</li>
-        @endforeach
-      </ul>
-    </div>
+    <p>入力に問題があります。再入力してください。</p>
   @endif
 
   <table>
     <form action="/hello" method="post">
       {{ csrf_field() }}
+
+      @if ($errors->has('name'))
+        <tr>
+          <th>ERROR</th>
+          <td>{{ $errors->first('name') }}</td>
+        </tr>
+      @endif
       <tr>
         <th>name:</th>
         <td><input type="text" name="name" value="{{old('name')}}"></td>
       </tr>
+
+      @if ($errors->has('mail'))
+        <tr>
+          <th>ERROR</th>
+          <td>{{ $errors->first('mail') }}</td>
+        </tr>
+      @endif
       <tr>
         <th>mail:</th>
         <td><input type="text" name="mail" value="{{old('mail')}}"></td>
       </tr>
+
+      @if ($errors->has('age'))
+        <tr>
+          <th>ERROR</th>
+          <td>{{ $errors->first('age') }}</td>
+        </tr>
+      @endif
       <tr>
         <th>age: </th>
         <td><input type="text" name="age" value="{{old('age')}}"></td>
       </tr>
+
       <tr>
         <td><input type="submit" value="send"></td>
       </tr>
