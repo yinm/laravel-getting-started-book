5de15107bf78fa5f4a7d5de9910766c60045367f
yinm <yinm.cminor9@gmail.com>
2017-09-18 16:34:20 +0900

    Add update

--- laravelapp/app/Http/Controllers/PersonController.php
+++ laravelapp/app/Http/Controllers/PersonController.php
@@ -50,4 +50,22 @@ class PersonController extends Controller
 
         return redirect('/person');
     }
+
+    public function edit(Request $request)
+    {
+        $person = Person::find($request->id);
+        return view('person.edit', ['form' => $person]);
+    }
+
+    public function update(Request $request)
+    {
+        $this->validate($request, Person::$rules);
+
+        $person = Person::find($request->id);
+        $form = $request->all();
+        unset($form['_token']);
+        $person->fill($form)->save();
+
+        return redirect('/person');
+    }
 }
--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -31,3 +31,5 @@ Route::get('person/find', 'PersonController@find');
 Route::post('person/find', 'PersonController@search');
 Route::get('person/add', 'PersonController@add');
 Route::post('person/add', 'PersonController@create');
+Route::get('person/edit', 'PersonController@edit');
+Route::post('person/edit', 'PersonController@update');
